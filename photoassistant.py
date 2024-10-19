fb30bd97ad8553c2a9eaaa57da8812129fb30a93
Abstract-AA <arthur_afonso@protonmail.com>
2024-10-19 23:33:09 +0900

    Update photoassistant.py

--- photoassistant.py
+++ photoassistant.py
@@ -2,7 +2,7 @@
 import gi
 gi.require_version("Gtk", "3.0")
 gi.require_version("Rsvg", "2.0")
-from gi.repository import Gtk, GObject, GdkPixbuf, GLib, Gdk
+from gi.repository import Gtk, GObject, GdkPixbuf, GLib, Gdk, Rsvg
 from moviepy.editor import VideoFileClip
 import os
 import time
@@ -56,6 +56,9 @@ class photoassistant(Gtk.Window):
         output_directory_button = Gtk.Button(label="Select Output Folder")
         output_directory_button.connect("clicked", self.on_select_output_directory)
         grid.attach(output_directory_button, 2, 1, 1, 1)
+
+        self.status_label = Gtk.Label(label="")
+        grid.attach(self.status_label, 3, 1, 1, 1)
 
         # Horizontal box to hold two scrollable windows
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
@@ -114,10 +117,7 @@ class photoassistant(Gtk.Window):
 
         grid.attach(hbox_controls3, 4, 1, 2, 1)
 
-        self.status_label = Gtk.Label(label="")
-        grid.attach(self.status_label, 0, 5, 3, 1)
-
-        #a sidebar for further options and fine tuning adjustments could be included, consider this later
+        # A sidebar for further options and fine tuning adjustments could be included, consider this later
     
     def on_select_input_file(self, widget):
         dialog = Gtk.FileChooserDialog(
@@ -158,7 +158,7 @@ class photoassistant(Gtk.Window):
         self.load_images_from_files(image_paths)
     
     def load_images_from_files(self, files):
-        # Load images into the first thumbnail grid
+        # load images into the first thumbnail grid
         for idx, file in enumerate(files):
             self.add_thumbnail(file, self.thumbnail_grid1, idx)
 
@@ -208,7 +208,77 @@ class photoassistant(Gtk.Window):
             self.thumbnail_grid2.remove(child)
 
     def cluster(self, widget):
-       pass
+        files = self.input_entry.get_text().split(", ")
+        if not files:
+            self.update_status("No images to cluster.")
+            return
+
+        # Clear the second grid before clustering (runs on main thread)
+        for child in self.thumbnail_grid2.get_children():
+            self.thumbnail_grid2.remove(child)
+
+        # Update status to inform user that clustering has started
+        self.update_status(f"Clustering Images...")
+
+        # Run the clustering process in a background thread
+        thread = threading.Thread(target=self.cluster_images_thread, args=(files,))
+        thread.start()
+
+    def cluster_images_thread(self, image_paths):     # this function runs in a separate thread to avoid frezing the GUI
+        clusters = self.cluster_images(image_paths)
+
+        # Pass the result back to the main thread to update the GUI
+        GLib.idle_add(self.display_clusters, clusters)
+
+    def display_clusters(self, clusters): # Updates the GUI with clustered images,runs on the main thread tho
+        row = 0
+
+        for cluster in clusters:
+            # Add a horizontal separator between clusters
+            if row > 0:
+                separator = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
+                self.thumbnail_grid2.attach(separator, 0, row, 4, 1)
+                row += 1
+
+            for idx, image_path in enumerate(cluster):
+                self.add_thumbnail(image_path, self.thumbnail_grid2, row * 4 + idx)
+
+            row += (len(cluster) // 4) + 1  # Move to next row for new cluster
+
+        # Update the status to inform the user clustering is done
+        self.update_status(f"Clustering completed. {len(clusters)} clusters found.")
+
+    def cluster_images(self, image_paths):
+        # this funtion clusters images based on pixel similarity. Returns a list of clusters (each cluster is a list of image paths).
+        def are_similar(img1, img2):
+            # use basic image comparison (e.g., mean pixel difference or delta E)
+            img1 = Image.open(img1).resize((200, 200)) # these values can severely slow down performance
+            img2 = Image.open(img2).resize((200, 200))
+
+            # Convert to RGB if necessary
+            if img1.mode != "RGB":
+                img1 = img1.convert("RGB")
+            if img2.mode != "RGB":
+                img2 = img2.convert("RGB")
+
+            pixels1 = list(img1.getdata())
+            pixels2 = list(img2.getdata())
+
+            # compute the mean pixel difference or delta E
+            diff = sum(get_delta_e(p1, p2) for p1, p2 in zip(pixels1, pixels2)) / len(pixels1)
+            return diff < 15  # Threshold for similarity, TODO add this to settings later, scores above 10 seem to be ok
+
+        clusters = []
+        for image_path in image_paths:
+            added = False
+            for cluster in clusters:
+                if are_similar(image_path, cluster[0]):
+                    cluster.append(image_path)
+                    added = True
+                    break
+            if not added:
+                clusters.append([image_path])
+        return clusters
     
     def on_open_settings(self,widget):
         # this creates a dialog window for settings
